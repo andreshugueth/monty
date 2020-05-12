c0f4fa474464cbc9f58f59ae432bb030fea0a300
samgj18 <samgj_18@hotmail.com>
2020-05-12 17:54:35 +0000

    Creating main files

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * ===================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Function that handles all events of monty
+ *
+ *        Version:  1.0
+ *        Created:  05/12/2020 04:56:45 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  SAMUEL GOMEZ Y ANDRES HUGUET
+ *   Organization:  Holberton
+ *
+ * ===================================================
+ */
+
+#include "monty.h"
+
+/**
+ * main - Entry Point
+ * @argc: Arguments count
+ * @argv: Array with actual arguments
+ * Description: Entry Point
+ * Return: EXIT_SUCCESS on success and EXIT_FAILURE otherwise
+ */
+int main(int argc, char *argv[])
+{
+
+	return (EXIT_SUCCESS);
+}			/* ----------  end of function main  ---------- */
