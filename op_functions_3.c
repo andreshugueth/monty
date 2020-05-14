55288113d9a486df99e6c683cbb7a3e8b48419bf
samgj18 <samgj_18@hotmail.com>
2020-05-14 18:18:52 +0000

    Making the mul function

---
+++ op_functions_3.c
@@ -0,0 +1,45 @@
+/*
+ * ===================================================
+ *
+ *       Filename:  op_functions_3.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  05/14/2020 06:17:36 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  SAMUEL GOMEZ J & ANDRES HUGUET
+ *   Organization:  Holberton
+ *
+ * ===================================================
+ */
+
+#include "monty.h"
+/**
+ * mul - mul the top two elements of the stack
+ * @stack: Top of the Linked list
+ * @line_num: Number of read line
+ *
+ * Return: Nothing
+ */
+void mul(stack_t **stack, unsigned int line_num)
+{
+	int aux;
+	stack_t *tmp;
+
+	tmp = (*stack);
+
+	if (!stack || !*stack || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_num);
+		free_list(*stack);
+		exit(EXIT_FAILURE);
+	}
+	aux = (*stack)->n;
+	aux *= (*stack)->next->n;
+	(*stack)->next->n = aux;
+	*stack = (*stack)->next;
+	free(tmp);
+}
